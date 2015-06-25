9d3f1335fafe13658a7f33dffc433130f884b699
gtors <gtors.potato@gmail.com>
2015-06-25 17:48:06 +0300

    Add another bits_counter

--- Rust/bits_counter/src/main.rs
+++ Rust/bits_counter/src/main.rs
@@ -7,16 +7,27 @@ static MASKS: [u64; 6] = [
     0x00000000ffffffff
 ];
 
-fn bits_count(mut val:u64) -> u8 {
+fn bits_count1(mut val:u64) -> u8 {
     for i in 0..6 {
         let shift = 1 << i;
         let mask = MASKS[i];
         val = (val & mask) + ((val >> shift) & mask)
     }
 
-    val as u8   
+    val as u8 
+}
+
+fn bits_count2(mut val:u64) -> u8 {
+    let mut acc:u8 = 0;
+    while val != 0 {
+        acc += 1;
+        val = val & (val - 1);
+    }
+
+    acc
 }
 
 fn main() {
-    println!("Bits count: {}", bits_count(7));
+    let num = 45635645_u64;
+    assert_eq!(bits_count1(num), bits_count2(num));
 }
